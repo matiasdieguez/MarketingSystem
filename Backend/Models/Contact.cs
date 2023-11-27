2338e303f2fb88d5548919afb8af0c8cde323b6c
Matías Dieguez <matiasdieguez@hotmail.com>
2023-11-27 20:56:01 -0300

    Agregar direccion a contacto

--- Backend/Models/Contact.cs
+++ Backend/Models/Contact.cs
@@ -6,6 +6,7 @@ namespace MarketingSystem.Backend.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime BirthDate { get; set; }
+        public string Address { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public double Age
